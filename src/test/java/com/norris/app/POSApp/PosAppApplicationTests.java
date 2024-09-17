2dbddd1bc0b24046f6c43d9f0adf2ace73763dc5
mwiinganorris <mwiinganorris@gmail.com>
2024-09-17 09:58:50 +0200

    Initial setup

---
+++ src/test/java/com/norris/app/POSApp/PosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norris.app.POSApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
